b9e28ba5cd317cddc8baa1d15656e211e7e7cc3e
Andrey-Shifanov <107719776+Andrey-Shifanov@users.noreply.github.com>
2022-06-17 14:39:15 -0700

    Add files via upload

---
+++ lab 11/Ex2/Starter/MeasuringDevice/HeartBeatEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace MeasuringDevice
+{
+
+    // Delegate defining the HeartBeat event signature.
+    public delegate void HeartBeatEventHandler
+     (object sender, HeartBeatEventArgs args);
+
+
+    public class HeartBeatEventArgs : EventArgs
+    {
+        public DateTime TimeStamp { get; private set; }
+
+        public HeartBeatEventArgs()
+            : base()
+        {
+            this.TimeStamp = DateTime.Now;
+        }
+    }
+}
